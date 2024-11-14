c0a25a947153d6e4e7cad983bc007467ad36e1d7
Demyanov69 <kukusyvika@gmail.com>
2024-11-14 23:37:34 +0300

    Add files via upload

---
+++ test/code.js
@@ -0,0 +1,105 @@
+window.addEventListener("DOMContentLoaded", function () {
+    const openBtn = document.getElementById("open-popup");
+    let popup = document.getElementById("popup");
+    openBtn.addEventListener("click", function () {
+        popup.style.display = "block";
+        window.history.pushState("", "", "form.html");
+    });
+
+    document.getElementById("close-popup").addEventListener("click", function () {
+        popup.style.display = "none";
+    });
+
+    window.onload = function () {
+        const storedFio = localStorage.getItem("field-fio");
+        const storedEmail = localStorage.getItem("field-email");
+        const storedMessage = localStorage.getItem("field-message");
+        const storedNumber = localStorage.getItem("field-number");
+        const storedOrg = localStorage.getItem("field-org");
+
+        if (storedFio) {
+            document.getElementsByName("field-fio")[0].value = storedFio;
+        }
+        if (storedEmail) {
+            document.getElementsByName("field-email")[0].value = storedEmail;
+        }
+        if (storedMessage) {
+            document.getElementsByName("field-message")[0].value = storedMessage;
+        }
+        if (storedNumber) {
+            document.getElementsByName("field-number")[0].value = storedNumber;
+        }
+        if (storedOrg) {
+            document.getElementsByName("field-org")[0].value = storedOrg;
+        }
+    };
+
+    const form = document.getElementById("feedback-form");
+
+    form.addEventListener("input", function () {
+        const fields = [
+            "field-fio",
+            "field-email",
+            "field-number",
+            "field-org",
+            "field-message"
+        ];
+        fields.forEach(function (field) {
+            localStorage.setItem(
+                field,
+                document.getElementsByName(field)[0].value
+            );
+        });
+    });
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        let email = document.getElementsByName("field-email");
+        let name = document.getElementsByName("field-fio");
+        let number = document.getElementsByName("field-number");
+        const checkbox = document.getElementById("consent");
+        let formcheck = true;
+        if (!name[0].value) {
+            formcheck = false;
+        }
+        if (!email[0].value) {
+            formcheck = false;
+        }
+        if (!number[0].value) {
+            formcheck = false;
+        }
+        if (!checkbox.checked) {
+            formcheck = false;
+        }
+
+        if (formcheck) {
+            $.ajax({
+                complete: function () {
+                    form.reset();
+                },
+                contentType: false,
+                data: new FormData(this),
+                dataType: "json",
+                error: function (jqXHR) {
+                    const errorObject = jqXHR.responseJSON;
+                    alert("Ошибка: " + errorObject.message);
+                },
+                processData: false,
+                success: function (response) {
+                    if (response.status === "success") {
+                        alert("Форма отправлена!");
+                        form.reset();
+                    } else {
+                        alert("Ошибка");
+                        form.reset();
+                    }
+                },
+                type: "POST",
+                url: "https://formcarry.com/s/0fgxFNQlfzy"
+            });
+        } else {
+            alert("Заполните поля формы");
+        }
+    });
+});
